9ff6791d35a8455a2c4cb26c5e0d99d86be1e55f
Eliel Haouzi <eliel.haouzi@gmail.com>
2021-05-25 14:19:24 +0300

    update elixir minimal version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Padlock.MixProject do
     [
       app: :padlock,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
