ed0a711aa30ca6c8bcf6479d6c4d33d527b7b92b
Laetitia <l.ohayon@bevaday.com>
2021-06-22 14:45:48 +0300

    fix mutexes

--- lib/padlock/mutexes.ex
+++ lib/padlock/mutexes.ex
@@ -1,35 +1,25 @@
 defmodule Padlock.Mutexes do
   import Ecto.Query, only: [lock: 2]
 
-  alias Ecto.Multi
-
   alias Padlock.Mutexes.Mutex
 
   @spec lock!(binary) :: Mutex.t()
   def lock!(name) when is_binary(name) do
-    Multi.new()
-    |> Multi.put(:current_mutex, get_mutex_by(name: name))
-    |> Multi.run(:mutex, fn
-      _, %{current_mutex: %Mutex{} = mutex} ->
-        {:ok, mutex}
+    {:ok, mutex} =
+      Padlock.repo().transaction(fn ->
+        create_if_not_exist_mutex!(name)
+        acquire_mutex!(name)
+      end)
 
-      _, %{current_mutex: nil} ->
-        _ = create_mutex(name)
-
-        {:ok, get_mutex_by(name: name)}
-    end)
-    |> Padlock.repo().transaction()
-    |> case do
-      {:ok, %{mutex: mutex}} -> mutex
-    end
+    mutex
   end
 
-  defp get_mutex_by(name: name) when is_binary(name),
-    do: Mutex |> lock("FOR UPDATE") |> Padlock.repo().get_by(name: name)
+  defp acquire_mutex!(name) when is_binary(name),
+    do: Mutex |> lock("FOR UPDATE") |> Padlock.repo().get_by!(name: name)
 
-  defp create_mutex(name) when is_binary(name) do
+  defp create_if_not_exist_mutex!(name) when is_binary(name) do
     %Mutex{}
     |> Mutex.changeset(%{name: name})
-    |> Padlock.repo().insert()
+    |> Padlock.repo().insert!(on_conflict: :nothing)
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,3 @@
 {:ok, _pid} = Padlock.TestRepo.start_link()
-:ok = Ecto.Adapters.SQL.Sandbox.checkout(Padlock.TestRepo)
-Ecto.Adapters.SQL.Sandbox.mode(Padlock.TestRepo, {:shared, self()})
 
 ExUnit.start()
